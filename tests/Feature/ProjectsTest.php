77a230c09d19609e130064ebdd0f96160ffbb02a
atif <atif@monotote.com>
2019-01-26 17:22:02 +0500

    Validation added

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -27,4 +27,19 @@ class ProjectsTest extends TestCase
         $this->get('/projects')->assertSee($attribures['title']);
 
     }
+
+    /** @test */
+    public function a_project_requires_a_title(){
+
+        $attributes = factory('App\Project')->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_requires_description(){
+
+        $attributes = factory('App\Project')->raw(['description' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
 }
